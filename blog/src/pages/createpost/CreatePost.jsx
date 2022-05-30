10ff258eb629e514068c6f3973123a834c6e36dc
castrintt <igordc38@gmail.com>
2022-05-30 16:35:48 -0300

    [add] post to firebase

--- blog/src/pages/createpost/CreatePost.jsx
+++ blog/src/pages/createpost/CreatePost.jsx
@@ -1,83 +1,116 @@
-import styles from './CreatePost.module.css'
+import styles from "./CreatePost.module.css";
 
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useAuthValue } from '../../context/AuthContext'
-import { useAuthentication } from '../../hooks/useAuthentication'
+import { useState } from "react";
+import { useInsertDocument } from "../../hooks/useInsertDocuments";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
 
-  const [title, setTitle] = useState('')
-  const [image, setImage] = useState('')
-  const [body, setBody] = useState('')
-  const [tags, setTags] = useState([])
-  const [formError, setFormError] = useState('')
+  const { user } = useAuthValue();
 
-  const {loading, error} = useAuthentication()
+  const { insertDocument, response } = useInsertDocument("posts");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+    setFormError("");
+
+    // validate image
+
+    // create tags array
+
+    // check values
+
+
+    console.log({
+      title,
+      image,
+      body,
+      tags: tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // if(formError) return
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags: tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // redirect to home page
+  };
 
   return (
     <div className={styles.create_post}>
-      <h1>Criar post</h1>
-      <p>Escreva sobre o que você quiser e compartilhe o seu conhecimento!</p>
-      <form
-        onSubmit={handleSubmit}
-      >
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
         <label>
-          <span>
-            Título:
-          </span>
+          <span>Título:</span>
           <input
             type="text"
-            name="title"
-            placeholder='Pense em um título...'
+            name="text"
+            required
+            placeholder="Pense num bom título..."
             onChange={(e) => setTitle(e.target.value)}
-            value={title} />
+            value={title}
+          />
         </label>
         <label>
-          <span>
-            URL da imagem:
-          </span>
+          <span>URL da imagem:</span>
           <input
             type="text"
             name="image"
-            placeholder='Insira a Url da imagem do seu post'
+            required
+            placeholder="Insira uma imagem que representa seu post"
             onChange={(e) => setImage(e.target.value)}
-            value={image} />
+            value={image}
+          />
         </label>
         <label>
-          <span>
-            Conteúdo:
-          </span>
+          <span>Conteúdo:</span>
           <textarea
             name="body"
             required
-            cols="30"
-            rows="3"
-            placeholder='Insira o conteúdo do post'
+            placeholder="Insira o conteúdo do post"
             onChange={(e) => setBody(e.target.value)}
-            value={body}></textarea>
+            value={body}
+          ></textarea>
         </label>
         <label>
-          <span>
-            Tags:
-          </span>
+          <span>Tags:</span>
           <input
             type="text"
             name="tags"
-            placeholder='Insira as tags do seu post separadas por vírgula'
+            required
+            placeholder="Insira as tags separadas por vírgula"
             onChange={(e) => setTags(e.target.value)}
-            value={tags} />
+            value={tags}
+          />
         </label>
-        {loading === false ? (<button className='btn'>Criar post</button>) : (<button className='btn' disabled>Aguarde...</button>)}
-
-                {error && <p className={styles.error}>{error}</p>}
+        {!response.loading && <button className="btn">Criar post</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde.. .
+          </button>
+        )}
+        {(response.error || formError) && (
+          <p className="error">{response.error }</p> 
+          // || formError
+        )}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreatePost
+export default CreatePost;
